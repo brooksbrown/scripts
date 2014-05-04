fa3e13972319250c21d872b763c0deaa29e28c5c
Brooks Brown <brooks.david.brown@gmail.com>
2014-05-04 01:12:58 -0400

    updates to phplu

--- phplu/phplu.py
+++ phplu/phplu.py
@@ -2,8 +2,14 @@ from bs4 import BeautifulSoup
 import urllib.request
 import os
 import sys
+import argparse
 
-page = urllib.request.urlopen('http://php.net/function.' + sys.argv[1])
+parser = argparse.ArgumentParser()
+parser.add_argument('function_title')
+parser.add_argument('--full', action='store_true')
+args = parser.parse_args()
+
+page = urllib.request.urlopen('http://php.net/function.' + args.function_title)
 html = page.read()
 soup = BeautifulSoup(html)
 refpurpose = soup.find_all("p", attrs={"class": "refpurpose"})[0]
@@ -12,8 +18,10 @@ desc = refpurpose.find_all('span', class_='dc-title')[0].get_text()
 synopsis = soup.find_all('div', class_='methodsynopsis')[0].get_text().replace('\n', '')
 synopsis = ' '.join(synopsis.split())
 comment = soup.find_all('p', class_='rdfs-comment')[0].get_text()
-parameters = soup.find_all('div', class_='refsect1 parameters')[0].get_text()
-parameters = os.linesep.join([s for s in parameters.splitlines() if s])
-print('\n\n' + title + ' - ' + desc + '\n\n' + synopsis + '\n' + comment + '\n\n' + parameters)
+print('\n\n' + title + ' - ' + desc + '\n\n' + synopsis + '\n' + comment)
+if args.full:
+    parameters = soup.find_all('div', class_='refsect1 parameters')[0].get_text()
+    parameters = os.linesep.join([s for s in parameters.splitlines() if s])
+    print('\n\n' + parameters)
 
 
